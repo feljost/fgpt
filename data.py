ec3f6d7ba1eac91121ab05093efabc7cff139e55
Felix Jost <fel.jost@gmail.com>
2025-07-06 11:36:50 +0000

    better error handling and simplify loader

--- data.py
+++ data.py
@@ -12,10 +12,10 @@ def load_tokens(file_path):
 
 
 class DataLoader:
-    def __init__(self, B, T, process_rank = 0, split='train'):
+    def __init__(self, B, T, split='train'):
+        # Note: does not work for multi-gpu training
         self.B = B
         self.T = T
-        self.process_rank = process_rank
         assert split in ['train', 'val'], "split must be either 'train' or 'val'"
 
         data_root = "fgpt/edu_fineweb10B"
@@ -25,7 +25,7 @@ class DataLoader:
         
         self.current_shard_index = 0
         self.tokens = load_tokens(os.path.join(data_root, self.shards[self.current_shard_index]))
-        self.current_position = self.B * self.T * self.process_rank  # start at a different position for each process
+        self.current_position = self.B * self.T
         self.current_position = 0 
 
     def next_batch(self):
@@ -35,9 +35,9 @@ class DataLoader:
         y = buf[1:].view(B, T)  # labels
         self.current_position += B * T
         if self.current_position + (B * T + 1) > len(self.tokens):
-            self.current_position = B * T * self.process_rank  # reset to the start of the next shard
+            self.current_position = B * T
             self.current_shard_index += 1
             if self.current_shard_index >= len(self.shards):
                 self.current_shard_index = 0
         
-        return x, y
+        return x, y, self.current_shard_index
--- train.py
+++ train.py
@@ -15,64 +15,97 @@ from model import B, T
 from data import DataLoader
 from inference import model_inference
 
-
-model = GPT(GPTConfig())
-model.eval()  # set to eval mode (would disable dropout, etc., but we don't have any in this model)
-model.to("cuda")  # move model to GPU
-
-torch.set_float32_matmul_precision("medium")
-dataloader = DataLoader(B, T, process_rank=0, split="train")  # create a dataloader
-
-model = torch.compile(model)
-optimizer = torch.optim.AdamW(
-    model.parameters(), lr=1e-4, betas=(0.9, 0.95), eps=1e-8
-)  # optimizer
-scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(optimizer, T_max=100_000)
 now_str = datetime.now().strftime("%Y%m%d_%H%M")
 
 
-for i in range(100_000):
-    t0 = time.time()
-    x, y = dataloader.next_batch()
-    x, y = x.to("cuda"), y.to("cuda")
-    optimizer.zero_grad()  # start with zero gradients
-    with torch.autocast("cuda", dtype=torch.bfloat16):
-        logits, loss = model(x, y)  # (B, T, vocab_size)
-    loss.backward()  # compute gradients
-    norm = torch.nn.utils.clip_grad_norm_(
-        model.parameters(), 1.0
-    )  # clip gradients to avoid exploding gradients
-    optimizer.step()  # update weights and biases
-    scheduler.step()  # update learning rate
+def train(
+    num_steps,
+    model,
+    dataloader,
+    optimizer,
+    scheduler,
+):
 
-    torch.cuda.synchronize()
-    t1 = time.time()
-    tokens_per_second = B * T / (t1 - t0)
-    if i % 5 == 0:
-        print(
-            f"Step: {i} | Loss: {loss.item():.4f} | norm {norm:.4f} | tokens/sec: {tokens_per_second:.2f} | time: {t1-t0:.3f} | lr {optimizer.param_groups[0]['lr']}"
+    print(f"Starting training for {num_steps} steps...")
+
+    for i in range(num_steps):
+        t0 = time.time()
+        x, y, shard_index = dataloader.next_batch()
+        x, y = x.to("cuda"), y.to("cuda")
+        optimizer.zero_grad()  # start with zero gradients
+        with torch.autocast("cuda", dtype=torch.bfloat16):
+            logits, loss = model(x, y)  # (B, T, vocab_size)
+        loss.backward()  # compute gradients
+        norm = torch.nn.utils.clip_grad_norm_(
+            model.parameters(), 1.0
+        )  # clip gradients to avoid exploding gradients
+        optimizer.step()  # update weights and biases
+        scheduler.step()  # update learning rate
+
+        torch.cuda.synchronize()
+        t1 = time.time()
+        tokens_per_second = B * T / (t1 - t0)
+        if i % 5 == 0:
+            print(
+                f"Step: {i} | Loss: {loss.item():.4f} | norm {norm:.4f} | t/s: {tokens_per_second:.2f} | time: {t1-t0:.3f} | lr {optimizer.param_groups[0]['lr']} | shard: {shard_index}"
+            )
+        if i % 10 == 0:
+            # Save metrics to a JSON file
+            metrics = {
+                "step": i,
+                "loss": float(loss.item()),
+                "norm": float(norm),
+                "tokens_per_second": float(tokens_per_second),
+                "lr": float(optimizer.param_groups[0]["lr"]),
+                "shard_index": shard_index,
+            }
+            with open(f"fgpt/train_metrics_{now_str}.jsonl", "a") as f:
+                f.write(json.dumps(metrics) + "\n")
+        if i % 200 == 0:
+            generated_tokens, decoded_output = model_inference(model=model)
+            print(f"Generated output: {decoded_output}")
+            # Save generated output to a separate JSONL file
+            sample = {
+                "step": i,
+                "decoded_output": decoded_output,
+            }
+            with open(f"fgpt/sample_outputs_{now_str}.jsonl", "a") as f:
+                f.write(json.dumps(sample) + "\n")
+
+    print("Training complete.")
+    torch.save(model.state_dict(), f"fgpt/model_weights_{now_str}.pth")
+
+
+if __name__ == "__main__":
+  
+    prev_model_weights = "fgpt/model_weights_20231030_1234.pth"
+    load_weights = False
+    if load_weights:
+        print(f"Loading model weights from {prev_model_weights}")
+        state_dict = torch.load(prev_model_weights, map_location="cuda")
+        model = GPT(GPTConfig())
+        model.load_state_dict(state_dict)
+    else:
+        model = GPT(GPTConfig())
+    
+    model.to("cuda")
+
+    torch.set_float32_matmul_precision("medium")
+    dataloader = DataLoader(B, T, split="train")  # create a dataloader
+
+    model = torch.compile(model)
+    optimizer = torch.optim.AdamW(
+        model.parameters(), lr=1e-4, betas=(0.9, 0.95), eps=1e-8
+    )  # optimizer
+    scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(optimizer, T_max=100_000)
+    try:
+        train(
+            num_steps=100_000,
+            model=model,
+            dataloader=dataloader,
+            optimizer=optimizer,
+            scheduler=scheduler,
         )
-    if i % 10 == 0:
-        # Save metrics to a JSON file
-        metrics = {
-            "step": i,
-            "loss": float(loss.item()),
-            "norm": float(norm),
-            "tokens_per_second": float(tokens_per_second),
-            "lr": float(optimizer.param_groups[0]["lr"]),
-        }
-        with open(f"train_metrics_{now_str}.jsonl", "a") as f:
-            f.write(json.dumps(metrics) + "\n")
-    if i % 200 == 0:
-        generated_tokens, decoded_output = model_inference(model=model)
-        print(f"Generated output: {decoded_output}")
-        # Save generated output to a separate JSONL file
-        sample = {
-            "step": i,
-            "decoded_output": decoded_output,
-        }
-        with open(f"sample_outputs_{now_str}.jsonl", "a") as f:
-            f.write(json.dumps(sample) + "\n")
-
-print("Training complete.")
-torch.save(model.state_dict(), f"model_weights_{now_str}.pth")
+    except Exception as e:
+        print(f"An error occurred during training: {e}")
+        torch.save(model.state_dict(), f"fgpt/model_weights_{now_str}_error.pth")
