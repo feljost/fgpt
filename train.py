f400ccb693bf9646457cf2adcc99a7985f25bd9a
Felix Jost <fel.jost@gmail.com>
2025-10-04 08:53:59 +0000

    much lower LR as overfitting was an issue

--- train.py
+++ train.py
@@ -174,8 +174,8 @@ if __name__ == "__main__":
     model = GPT(GPTConfig())
     model.to("cuda")
     current_step = 0
-    max_steps = 500_000 + 1
-    start_lr = 1e-4
+    max_steps = 200_000 + 1
+    start_lr = 5e-5
     min_lr = 0.05 * start_lr
 
     # use this to restart training from a specific spot
@@ -183,7 +183,7 @@ if __name__ == "__main__":
     load_weights = False
 
     optimizer = torch.optim.AdamW(
-        model.parameters(), lr=1e-4, betas=(0.9, 0.95), eps=1e-8
+        model.parameters(), lr=start_lr, betas=(0.9, 0.95), eps=1e-8, weight_decay=0.1
     )
     scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(
         optimizer, T_max=max_steps, eta_min=min_lr
